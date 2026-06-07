d3a5136507a2e247c7f708393dd1ea975bfa9f6a
unknown <manjirikench7@gmail.com>
2026-06-07 21:56:39 +0530

    Add Hospital Mongoose model with fields, validation and geospatial index

---
+++ server/models/Hospital.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+
+const specialistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['cardiologist' , 'trauma surgeon', 'burn specialist', 'neurologist','general']
+    },
+    available: {
+        type: Boolean, 
+        default: false
+    }
+    
+});
+
+const hospitalSchema = new mongoose.Schemama({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+        match: /^[0-9]{10}$/
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+    coordinates: {
+        type: [Number],
+        required: true
+    } 
+    },
+    emergencyTypes: {
+        type: [String],
+        enum: ['cardiac','trauma','burns','general'],
+        required: true
+    },
+    totalICUBeds: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    availableICUBeds: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    emergencydeptopen:{
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    currentload: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10,
+        default: 0
+    },
+    lastupdated: {
+        type: Date,
+        ddefault: Date.now
+    }
+});
+hospitalSchema.index({ location: '2dsphere'});
+
+const Hospital = mongoose.model('Hospital', hospitalSchema);
+module.exports = Hospital;
