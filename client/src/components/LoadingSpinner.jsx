5f911374df30ea988f89e7d97f7d54c2f41862b4
unknown <manjirikench7@gmail.com>
2026-07-07 14:27:27 +0530

    Update LoadingSpinner with animated CSS spinner

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -1,8 +1,10 @@
-function LoadingSpinner(){
-    return(
-        <div>
-            <p>Loading...</p>
-        </div>
-    )
+import './LoadingSpinner.css'
+
+function LoadingSpinner() {
+return (
+    <div className="spinner-container">
+        <div> className="spinner"</div>
+    </div>
+)
 }
 export default LoadingSpinner
